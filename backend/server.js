69bc9f161edc77e94f0b8a12987c40d3fbd35cd5
Luis_codigos <163169558+luisaoaoao@users.noreply.github.com>
2025-06-16 12:50:37 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,29 @@
-import server from './lib/app.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import ffi from 'ffi-napi';
+import ref from 'ref-napi';
+import StructType from 'ref-struct-napi';
 
-const PORT = 8003;
+// Importa o caminho do arquivo e o diretório atual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-server.listen(PORT, () =>{
-        console.log('Server listening on http://localhost:' + PORT)
-    })
+// Criação da estrutura reading
+const Reading = StructType({
+  erro: 'int',
+  id_prova: 'int',
+  id_participante: 'int',
+  leitura: 'string'
+});
+
+// Caminho absoluto para a .so
+const libPath = path.join(__dirname, '..', 'lib', 'libleitor.so');
+
+// Carrega a biblioteca com os nomes corretos exportados pelo C
+const lib = ffi.Library(libPath, {
+  read_image_path: [Reading, ['string']],
+  read_image_data: [Reading, ['string', 'pointer', 'int']]
+});
+
+export default lib;
