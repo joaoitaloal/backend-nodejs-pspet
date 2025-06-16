4f022b697916e35dd9a6f85f20bd412d90439942
Luis_codigos <163169558+luisaoaoao@users.noreply.github.com>
2025-06-15 23:28:37 -0300

    Delete backend/lib directory

--- backend/lib/database/database.js
+++
@@ -1,7 +0,0 @@
-import { messages, syncDB } from "./models.js";
-
-export async function addMessage(message){
-    await syncDB();
-    await messages.create({name: message.name, message: message.message, date: message.date, time: message.time})
-    .catch((e) => console.error(e));
-}
